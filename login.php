560d005de89d65b66dd6abbb796213536efe5586
Lupoarticoblu <wolf94-@libero.it>
2024-08-28 13:31:23 +0200

    login utente

--- login.php
+++ login.php
@@ -1,5 +1,24 @@
 <?php
  include_once('partials/head.php');  
+ if(empty($_POST['email']) || empty($_POST['password'])) {
+   $errorMsg = 'Inserisci username e password';
+ }else{
+  $string = file_get_contents('./db/user.json');
+  $users = json_decode($string, true);
+  //solo se trovo l'utente richiesto il valore tornerà true
+  $validUser = false;
+  foreach ($users as $user) {
+    if($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']) {
+      $validUser = true;
+      
+      $logged = $user;
+  }
+  if($validUser){
+    $errorMsg = 'Login effettuato, benvenuto '. $logged['name'];
+  }else{
+    $errorMsg = 'Login fallito';
+  }  
+  }
 ?>
 <body>
  <?php
@@ -11,6 +30,7 @@
       <div class="row">
         <div class="col-md-12">
           <h1>Login</h1>
+          <h3><?php echo $errorMsg ?></h3>
           <!-- la action della form punta su se stesso, quindi riconosciamo tramite il var dump di $_SERVER la uri corrente -->
           <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <div class="mb-3">
